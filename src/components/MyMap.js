41d2f7e0ad48419d89b536393fa9747ebac5cc4b
Tino Brose <tino.brose92@gmail.com>
2020-12-14 15:11:20 +0100

    updated adjustments

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -11,7 +11,7 @@ export default function MyMap({ lng, lat }) {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker lng={-156.29} lat={20.94}>
+      <Marker position={position}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
