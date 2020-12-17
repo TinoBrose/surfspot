7686e6591ca9006aa312c6be9471bbe62cf6b76b
Tino Brose <tino.brose92@gmail.com>
2020-12-17 15:34:06 +0100

    navbar plus font

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from './NavbarElements';
+import Logo from './Surftrip.png'
 
 const Navbar = () => {
     return (
         <>
            <Nav>
                <NavLink to="/">
-                   <h1>Logo</h1>
+                    <img className='Logo' src={Logo} alt='Surftrip' />
                </NavLink>
                <Bars />
                <NavMenu>
-                   <NavLink to="/home" activeStyle>
+                   <NavLink to="/" exact activeStyle>
                        Home
                    </NavLink>
                    <NavLink to="/about" activeStyle>
